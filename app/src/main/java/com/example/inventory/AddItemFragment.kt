3196348a5392919b9bcdb45f1d8886dee5f698e1
muhammadjordansky <113664487+muhammadjordansky@users.noreply.github.com>
2022-12-29 21:44:15 +0700

    Initial commit

---
+++ app/src/main/java/com/example/inventory/AddItemFragment.kt
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.inventory
+
+import android.content.Context.INPUT_METHOD_SERVICE
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import com.example.inventory.databinding.FragmentAddItemBinding
+
+// Fragmen untuk menambah atau memperbarui item di database Inventaris.
+
+class AddItemFragment : Fragment() {
+
+    private val navigationArgs: ItemDetailFragmentArgs by navArgs()
+
+    // Mengikat instance objek yang sesuai dengan tata letak fragment_add_item.xml
+    // Properti ini bukan nol antara callback siklus hidup onCreateView() dan onDestroyView(),
+    // saat hierarki tampilan dilampirkan ke fragmen
+    private var _binding: FragmentAddItemBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentAddItemBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    //Dipanggil saat fragmen di destroy.
+    override fun onDestroyView() {
+        super.onDestroyView()
+        // Hide keyboard.
+        val inputMethodManager = requireActivity().getSystemService(INPUT_METHOD_SERVICE) as
+                InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(requireActivity().currentFocus?.windowToken, 0)
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/example/inventory/ItemDetailFragment.kt
@@ -0,0 +1,70 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.inventory
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.example.inventory.databinding.FragmentItemDetailBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+
+// [ItemDetailFragment] menampilkan detail item yang dipilih.
+
+class ItemDetailFragment : Fragment() {
+    private val navigationArgs: ItemDetailFragmentArgs by navArgs()
+
+    private var _binding: FragmentItemDetailBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentItemDetailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+   //Menampilkan dialog peringatan untuk mendapatkan konfirmasi pengguna sebelum menghapus item.
+    private fun showConfirmationDialog() {
+        MaterialAlertDialogBuilder(requireContext())
+            .setTitle(getString(android.R.string.dialog_alert_title))
+            .setMessage(getString(R.string.delete_question))
+            .setCancelable(false)
+            .setNegativeButton(getString(R.string.no)) { _, _ -> }
+            .setPositiveButton(getString(R.string.yes)) { _, _ ->
+                deleteItem()
+            }
+            .show()
+    }
+
+    //Menghapus item saat ini dan menavigasi ke fragmen daftar.
+    private fun deleteItem() {
+        findNavController().navigateUp()
+    }
+
+    //Dipanggil saat fragmen di destroy.
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
